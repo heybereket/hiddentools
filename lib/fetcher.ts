0f01827547598958539df674570bfb69c99e6921
Bereket Semagn <semagnbereket@gmail.com>
2022-01-12 20:35:39 -0500

    fix: add BASE_URL env variable

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,11 +1,13 @@
+import { BASE_URL } from './constants';
+
 export const fetcher = async (url: string, init: Partial<RequestInit> = {}) => {
-	return await fetch(`http://localhost:3000/api/${url}`, {
-		mode: 'cors',
-		...init,
-		credentials: 'include',
-		headers: {
-			'Content-Type': 'application/json',
-			...init.headers,
-		},
-	}).then(res => res.json());
+  return await fetch(`${BASE_URL}/api/${url}`, {
+    mode: 'cors',
+    ...init,
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+      ...init.headers,
+    },
+  }).then((res) => res.json());
 };
