106b09e2d45c3ba601d9f926476e42f1fc9f936d
Bereket Semagn <semagnbereket@gmail.com>
2022-01-15 12:16:50 -0500

    fix: title per page

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import { Header } from '../components/Header';
 import { Card } from '../components/Card';
 import Link from 'next/link';
 import { CATEGORIES } from '../lib/constants';
+import Head from 'next/head';
 
 interface HomeProps {
   tools: any;
@@ -39,82 +40,89 @@ const Home: React.FC<HomeProps> = (props) => {
   };
 
   return (
-    <div className="px-16 py-16 md:py-0">
-      <Header />
+    <>
+      <Head>
+        <title>Hidden Tools</title>
+      </Head>
+      <div className="px-16 py-16 md:py-0">
+        <Header />
 
-      <div className="flex space-x-4 mt-4">
-        <input
-          type="text"
-          placeholder="Search tools..."
-          value={query}
-          onChange={changeSearch}
-          spellCheck="false"
-          className="bg-secondary focus:outline-none focus:shadow-outline rounded-lg py-2 px-4 block w-full"
-        />
-        <Link href="/submit">
-          <a className="bg-secondary hover:bg-gray-700 text-center focus:outline-none focus:shadow-outline rounded-lg py-2 px-4 block w-full">
-            Submit a tool
-          </a>
-        </Link>
-      </div>
-
-      {!viewCategories && (
-        <button
-          className="block md:hidden text-gray-500 dark:text-primary-100 mt-4 font-semibold"
-          onClick={() => setViewCategories(true)}
-        >
-          Categories are hidden. View categories -&gt;
-        </button>
-      )}
-
-      <div
-        className={`space-y-3 mt-5 md:mt-2 ${
-          viewCategories ? 'block' : 'hidden md:block'
-        }`}
-      >
-        <button
-          className="bg-secondary hover:bg-gray-700 hover:cursor-pointer mr-3 focus:cursor-pointer px-3 py-2 rounded-lg text-xs cursor-auto"
-          onClick={() => setTools(props.tools.sort(() => Math.random() - 0.5))}
-        >
-          All ({props.tools.length})
-        </button>
-        {CATEGORIES.map((category: string) => (
-          <button
-            key={category}
-            onClick={() =>
-              setTools(
-                props.tools.filter((tool: Tool) => tool.category === category)
-              )
-            }
-            className="bg-secondary hover:bg-gray-700 hover:cursor-pointer mr-3 focus:cursor-pointer px-3 py-2 rounded-lg text-xs cursor-auto"
-          >
-            {category}{' '}
-            <span className="text-gray-400">
-              (
-              {
-                props.tools.filter((tool: Tool) => tool.category === category)
-                  .length
-              }
-              )
-            </span>
-          </button>
-        ))}
-      </div>
-
-      {tools.length === 0 && (
-        <div className="mt-5">
-          <span>
-            😥 No results found for <strong>{query}</strong>.
-          </span>
+        <div className="flex space-x-4 mt-4">
+          <input
+            type="text"
+            placeholder="Search tools..."
+            value={query}
+            onChange={changeSearch}
+            spellCheck="false"
+            className="bg-secondary focus:outline-none focus:shadow-outline rounded-lg py-2 px-4 block w-full"
+          />
+          <Link href="/submit">
+            <a className="bg-secondary hover:bg-gray-700 text-center focus:outline-none focus:shadow-outline rounded-lg py-2 px-4 block w-full">
+              Submit a tool
+            </a>
+          </Link>
         </div>
-      )}
 
-      <div className="md:grid sm:grid-cols-1 grid-cols-2 gap-4 gap-y-15 gap-x-5 md:grid-cols-4">
-        {tools.map((item: any, i: Key) => (
-          <Card key={i} {...item} />
-        ))}
+        {!viewCategories && (
+          <button
+            className="block md:hidden text-gray-500 dark:text-primary-100 mt-4 font-semibold"
+            onClick={() => setViewCategories(true)}
+          >
+            Categories are hidden. View categories -&gt;
+          </button>
+        )}
+
+        <div
+          className={`space-y-3 mt-5 md:mt-2 ${
+            viewCategories ? 'block' : 'hidden md:block'
+          }`}
+        >
+          <button
+            className="bg-secondary hover:bg-gray-700 hover:cursor-pointer mr-3 focus:cursor-pointer px-3 py-2 rounded-lg text-xs cursor-auto"
+            onClick={() =>
+              setTools(props.tools.sort(() => Math.random() - 0.5))
+            }
+          >
+            All ({props.tools.length})
+          </button>
+          {CATEGORIES.map((category: string) => (
+            <button
+              key={category}
+              onClick={() =>
+                setTools(
+                  props.tools.filter((tool: Tool) => tool.category === category)
+                )
+              }
+              className="bg-secondary hover:bg-gray-700 hover:cursor-pointer mr-3 focus:cursor-pointer px-3 py-2 rounded-lg text-xs cursor-auto"
+            >
+              {category}{' '}
+              <span className="text-gray-400">
+                (
+                {
+                  props.tools.filter((tool: Tool) => tool.category === category)
+                    .length
+                }
+                )
+              </span>
+            </button>
+          ))}
+        </div>
+
+        {tools.length === 0 && (
+          <div className="mt-5">
+            <span>
+              😥 No results found for <strong>{query}</strong>.
+            </span>
+          </div>
+        )}
+
+        <div className="md:grid sm:grid-cols-1 grid-cols-2 gap-4 gap-y-15 gap-x-5 md:grid-cols-4">
+          {tools.map((item: any, i: Key) => (
+            <Card key={i} {...item} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- pages/submit.tsx
+++ pages/submit.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Head from 'next/head';
 import { useState } from 'react';
 import { Header } from '../components/Header';
 import { CATEGORIES } from '../lib/constants';
@@ -27,68 +28,73 @@ const Tool = () => {
   };
 
   return (
-    <div className="px-16 py-16 md:py-0">
-      <Header />
+    <>
+      <Head>
+        <title>Submit / Hidden Tools</title>
+      </Head>
+      <div className="px-16 py-16 md:py-0">
+        <Header />
 
-      <div className="mt-3">
-        <label className="text-sm text-gray-400">
-          *title & description are optional*
-        </label>
-        {error && <p className="text-sm text-red-500">{error}</p>}
-      </div>
+        <div className="mt-3">
+          <label className="text-sm text-gray-400">
+            *title & description are optional*
+          </label>
+          {error && <p className="text-sm text-red-500">{error}</p>}
+        </div>
 
-      <div className="flex space-x-4 mt-4 mb-4">
-        <input
-          placeholder="Tool Name"
-          type="text"
-          spellCheck="false"
-          className="bg-secondary focus:outline-none focus:shadow-outline rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
-          onChange={(e: any) => setToolName(e.target.value)}
-        />
-        <input
-          placeholder="Tool URL"
-          type="text"
-          spellCheck="false"
-          className="bg-secondary focus:outline-none focus:shadow-outline rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
-          onChange={(e: any) => setToolLink(e.target.value)}
-        />
-      </div>
+        <div className="flex space-x-4 mt-4 mb-4">
+          <input
+            placeholder="Tool Name"
+            type="text"
+            spellCheck="false"
+            className="bg-secondary focus:outline-none focus:shadow-outline rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+            onChange={(e: any) => setToolName(e.target.value)}
+          />
+          <input
+            placeholder="Tool URL"
+            type="text"
+            spellCheck="false"
+            className="bg-secondary focus:outline-none focus:shadow-outline rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+            onChange={(e: any) => setToolLink(e.target.value)}
+          />
+        </div>
 
-      <div className="flex space-x-4 mt-4 mb-4">
-        <input
-          placeholder="Tool Description"
-          type="text"
-          spellCheck="false"
-          className="bg-secondary focus:outline-none focus:shadow-outline rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
-          onChange={(e: any) => setToolDesc(e.target.value)}
-        />
-        <select
-          onChange={(e) => setToolCategory(e.target.value)}
-          className="bg-secondary text-gray-400 focus:outline-none focus:shadow-outline rounded-lg py-2 px-4 block w-full"
-        >
-          <option selected disabled>
-            Category -&gt;
-          </option>
-          {CATEGORIES.sort().map((category: string) => (
-            <option key={category} value={category}>
-              {category}
+        <div className="flex space-x-4 mt-4 mb-4">
+          <input
+            placeholder="Tool Description"
+            type="text"
+            spellCheck="false"
+            className="bg-secondary focus:outline-none focus:shadow-outline rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+            onChange={(e: any) => setToolDesc(e.target.value)}
+          />
+          <select
+            onChange={(e) => setToolCategory(e.target.value)}
+            className="bg-secondary text-gray-400 focus:outline-none focus:shadow-outline rounded-lg py-2 px-4 block w-full"
+          >
+            <option selected disabled>
+              Category -&gt;
             </option>
-          ))}
-        </select>
+            {CATEGORIES.sort().map((category: string) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <button
+          onClick={submitTool}
+          className="bg-secondary hover:bg-gray-700 focus:outline-none focus:shadow-outline rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+        >
+          Submit Tool
+        </button>
+
+        <label className="flex items-center justify-center text-center mt-3 text-sm text-gray-400">
+          Upon submitting your tool, it will be required to pass verification
+          before being posted to Hidden Tools.
+        </label>
       </div>
-
-      <button
-        onClick={submitTool}
-        className="bg-secondary hover:bg-gray-700 focus:outline-none focus:shadow-outline rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
-      >
-        Submit Tool
-      </button>
-
-      <label className="flex items-center justify-center text-center mt-3 text-sm text-gray-400">
-        Upon submitting your tool, it will be required to pass verification
-        before being posted to Hidden Tools.
-      </label>
-    </div>
+    </>
   );
 };
 
